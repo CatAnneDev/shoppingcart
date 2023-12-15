29d9f7f0095704eb9ffe1959dc48ebeab51ea167
OpalDel <62034499+CatAnneDev@users.noreply.github.com>
2023-12-15 08:23:58 -0600

    reorganize, user login priv, pictures 640px

--- functions.php
+++ functions.php
@@ -1,21 +1,31 @@
 <?php
-function pdo_connect_mysql() 
-{
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USERNAME = 'root';
-    $DATABASE_PASSWORD = '';
-    $DATABASE_NAME = 'shoppingcart';
-
-    // if there is an error with the connection, exit and display error
-    try 
-    {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USERNAME, $DATABASE_PASSWORD);
-        echo "Connection to database successful.";
-    } 
-    catch (PDOException $exception) 
-    {
-    	exit('Failed to connect to database!');
-    }
+// Visitor header, if user is not logged in
+function unregistered_header($title) {
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>$title</title>
+            <link href="style.css" rel="stylesheet" type="text/css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        </head>
+        <body>
+            <header>
+                <div class="content-wrapper">
+                    <h1>Shopping 4 Pets</h1>
+                    <div class="link-icons">
+                        <a href="index.php?page=register">
+                            <i class="fas fa-user-plus"></i>
+                        </a>
+                        <a href="index.php?page=login">
+                            <i class="fas fa-sign-in-alt"></i>
+                        </a>
+                    </div>
+                </div>
+            </header>
+            <main>
+    EOT;
 }
 
 // Shop header, if user is logged in
@@ -52,35 +62,6 @@ echo <<<EOT
 EOT;
 }
 
-// Visitor header, if user is not logged in
-function unregistered_header($title) {
-    echo <<<EOT
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>$title</title>
-            <link href="style.css" rel="stylesheet" type="text/css">
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        </head>
-        <body>
-            <header>
-                <div class="content-wrapper">
-                    <h1>Shopping 4 Pets</h1>
-                    <div class="link-icons">
-                        <a href="index.php?page=register">
-                            <i class="fas fa-user-plus"></i>
-                        </a>
-                        <a href="index.php?page=login">
-                            <i class="fas fa-sign-in-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </header>
-            <main>
-    EOT;
-}
-
 // Shop footer
 function shop_footer() {
 echo <<<EOT
@@ -93,5 +74,34 @@ echo <<<EOT
     </body>
 </html>
 EOT;
+}
+
+// connect to MySQL database named shoppingcart via localhost
+function pdo_connect_mysql() 
+{
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USERNAME = 'root';
+    $DATABASE_PASSWORD = '';
+    $DATABASE_NAME = 'shoppingcart';
+
+    // if there is an error with the connection, exit and display error
+    try 
+    {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USERNAME, $DATABASE_PASSWORD);
+        echo "Connection to database successful.";
+    } 
+    catch (PDOException $exception) 
+    {
+    	exit('Failed to connect to database!');
+    }
+}
+
+// redirects logged out users to the login page
+function login_redirect()
+{
+    if ($_SESSION['loggedin'] != true)
+    {
+        header('Location: index.php');
+    }
 }
 ?>
